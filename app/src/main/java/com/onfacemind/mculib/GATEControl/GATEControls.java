ccc198980db03d7a6473263fd5ae674d76fec12a
LiuC <355994698@qq.com>
2018-11-12 11:44:43 +0800

    修复自动选择启动器问题

---
+++ app/src/main/java/com/onfacemind/mculib/GATEControl/GATEControls.java
@@ -0,0 +1,94 @@
+package com.onfacemind.aifacef1002.ExternalDevice.GATEControl;
+
+import android.util.Log;
+
+
+/**
+ * Created by 刘承. on 2018/4/24.
+ */
+
+public class GATEControls {
+    private static final String TAG = "GATEControls";
+    public static boolean init;
+    static GATEControls gateControls = new GATEControls();
+
+    public static GATEControls instance() {
+        return gateControls;
+    }
+
+    public boolean createSerialProt() {
+        init = SeriaProtController.instance().create("/dev/ttySAC3", 9600);//固定地址
+        return init;
+    }
+
+    //左开
+    public void leftopen() {
+        if (init)
+            sendMsg(0x40);
+        else Log.d(TAG, "leftopen: 发送失败 串口未初始化");
+    }
+
+    //右开
+    public void rightopen() {
+        if (init)
+            sendMsg(0x41);
+        else Log.d(TAG, "leftopen: 发送失败 串口未初始化");
+    }
+
+    void sendMsg(int queryCalss) {
+        byte[] msgBodyBytes = new byte[5];
+        msgBodyBytes[0] = integerTo1Byte(0xEB);//同步码
+        msgBodyBytes[1] = integerTo1Byte(0x01);//设备地址待定
+        msgBodyBytes[2] = integerTo1Byte(queryCalss);//命令码
+        msgBodyBytes[3] = integerTo1Byte(0);//命令码
+        byte xor = msgBodyBytes[0];
+        for (int i = 1; i < msgBodyBytes.length - 1; i++) {
+            xor ^= msgBodyBytes[i];
+        }
+        msgBodyBytes[4] = xor;//校验码
+        String s = toHexString(msgBodyBytes);
+        Log.d(TAG, "sendMsg: =" + s);
+        SeriaProtController.instance().sendCommand(msgBodyBytes);
+
+//4字节模式
+//        byte[] msgBodyBytes = new byte[4];
+//        msgBodyBytes[0] = integerTo1Byte(0xEB);//同步码
+//        msgBodyBytes[1] = integerTo1Byte(0x01);//设备地址待定
+//        msgBodyBytes[2] = integerTo1Byte(queryCalss);//命令码
+//        byte xor = msgBodyBytes[0];
+//        for (int i = 1; i < msgBodyBytes.length - 1; i++) {
+//            xor ^= msgBodyBytes[i];
+//        }
+//        msgBodyBytes[3] = xor;//校验码
+//        String s = toHexString(msgBodyBytes);
+//        Log.d(TAG, "sendMsg: =" + s);
+//        pinterText("sendMsg: =" + s);
+    }
+
+    /**
+     * 把一个整形该为byte
+     *
+     * @param value
+     */
+    public byte integerTo1Byte(int value) {
+        return (byte) (value & 0xFF);
+    }
+
+    public static String toHexString(byte[] bs) {
+        return new String(encodeHex(bs));
+    }
+
+    private static final char[] DIGITS_HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    protected static char[] encodeHex(byte[] data) {
+        int l = data.length;
+        char[] out = new char[l << 1];
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS_HEX[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS_HEX[0x0F & data[i]];
+        }
+        return out;
+    }
+
+//        new readThread(SeriaProtController.instance().getmInputStream()).start();//启动读取线程
+}
---
+++ app/src/main/java/com/onfacemind/mculib/GATEControl/SeriaProtController.java
@@ -0,0 +1,155 @@
+package com.onfacemind.aifacef1002.ExternalDevice.GATEControl;
+
+import android.serialport.SerialPort;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+
+/**
+ * Created by 刘承. on 2018/4/18.
+ * 创建 485 输出串口
+ * <p>
+ * SerialPort 使用卡尔jar中的
+ */
+
+public class SeriaProtController {
+    private static final String TAG = "SeriaProtController";
+    private static SeriaProtController seriaProtController;
+    private SerialPort mSerialPort = null;
+    private OutputStream mOutputStream;
+    private InputStream mInputStream;
+
+    public static SeriaProtController instance() {
+        if (seriaProtController == null) {
+            seriaProtController = new SeriaProtController();
+        }
+        return seriaProtController;
+    }
+
+    private SeriaProtController() {
+    }
+
+    /**
+     * 创建
+     */
+    public boolean create(String path, int baudrate) {
+        if (mSerialPort == null) {
+            try {
+                mSerialPort = getSerialPort(path, baudrate);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "SeriaProtController: 串口启动失败");
+                return false;
+            }
+        }
+        if (mSerialPort == null) return false;
+        return true;
+    }
+
+
+    /**
+     * 发送打印命令
+     *
+     * @param bytes
+     */
+    public void sendCommand(byte[] bytes) {
+        try {
+            if (mOutputStream != null) {
+                mOutputStream.write(bytes);
+                mOutputStream.flush();
+                Log.d(TAG, "sendCommand: Lib发送数据成功=" + Arrays.toString(bytes));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //sleep(100);
+    }
+
+    public InputStream getmInputStream() {
+        return mInputStream;
+    }
+
+    private SerialPort getSerialPort(String path, int baudrate) throws SecurityException, IOException, InvalidParameterException {
+        if (mSerialPort == null) {
+            /* Read serial port parameters */
+//			SharedPreferences sp = getSharedPreferences("android_serialport_api.sample_preferences", MODE_PRIVATE);
+//			String path = sp.getString("DEVICE", "");
+//			int baudrate = Integer.decode(sp.getString("BAUDRATE", "-1"));
+
+
+//            String path = "/dev/ttySAC3";//902闸机RS485协议控制
+//            int baudrate = 9600;//902闸机RS485协议波特率
+//            path = path;//902闸机RS485协议控制
+//            String path = "/dev/ttySAC1";
+//            int baudrate = 115200;
+//
+            /* Check parameters */
+//            if ((path.length() == 0) || (baudrate == -1)) {
+//                //throw new InvalidParameterException();
+//                /*use default value.    Nirvana 0710*/
+////				path = "/dev/ttyS1";
+//                path = "/dev/ttySAC1";
+//                baudrate = 115200;
+//            }
+
+            /* Open the serial port */
+//            mSerialPort = new SerialPort(new File(path), baudrate, 0);
+            try {
+                mSerialPort = new SerialPort(path, baudrate);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            try {
+                mOutputStream = mSerialPort.getOutputStream();
+                mInputStream = mSerialPort.getInputStream();
+
+                /* Create a receiving thread */
+//                mReadThread = new ReadThread();//暂不使用接收线程
+//                mReadThread.start();
+            } catch (SecurityException e) {
+                Log.e(TAG, "You do not have read/write permission to the serial port.");
+            } catch (InvalidParameterException e) {
+                Log.e(TAG, "Please configure your serial port first.");
+            }
+        }
+        return mSerialPort;
+    }
+
+    //读取线程
+    class readThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
+    public void closePrinter() {
+        if (mSerialPort != null) {
+            mSerialPort.close();
+            mSerialPort = null;
+        }
+        try {
+            if (mOutputStream != null) {
+                mOutputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (mInputStream != null) {
+                mInputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
